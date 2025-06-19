8ec564144589124695b7c800f360b421de6c9b67
Gabriel Mizuno <gabrielmizuno@gmail.com>
2025-06-19 13:10:53 -0300

    Hotfix etl

--- util/requester.py
+++ util/requester.py
@@ -7,5 +7,3 @@ if TYPE_CHECKING:
 def get_data(ticket: str, start: str, end: str) -> "pd.DataFrame":
     return yf.Ticker(ticket).history(start=start, end=end).reset_index()
 
-
-x = get_data("B5P211.SA", '2025-02-19', '2025-02-21')
