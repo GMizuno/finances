2dfafb12502801129774c59e061c121f7efb2ddb
Gabriel Mizuno <gabrielmizuno@gmail.com>
2025-12-06 15:44:36 -0300

    Adds IAM policy for Lambda to interact with S3, Glue Catalog, and Athena for Iceberg table operations, and attaches the policy to the execution role.

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -48,3 +48,66 @@ resource "aws_iam_role_policy_attachment" "kms_policy_attachment" {
   role       = aws_iam_role.lambda_execution_role.name
   policy_arn = aws_iam_policy.kms_decrypt_policy.arn
 }
+
+resource "aws_iam_policy" "lambda_iceberg_policy" {
+  name        = "lambda-iceberg-writer-policy"
+  description = "Permissoes para escrever tabelas Iceberg via AWS Wrangler"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "S3Access"
+        Effect = "Allow"
+        Action = [
+          "s3:PutObject",
+          "s3:GetObject",
+          "s3:DeleteObject",
+          "s3:ListBucket",
+          "s3:GetBucketLocation"
+        ]
+        Resource = [
+          "arn:aws:s3:::finance-605771322130",             # O Bucket em si
+          "arn:aws:s3:::finance-605771322130/raw/stock_data/*", # Local da Tabela
+          "arn:aws:s3:::finance-605771322130/temp/*"       # Local Temporário/Staging
+        ]
+      },
+
+      {
+        Sid    = "GlueCatalogAccess"
+        Effect = "Allow"
+        Action = [
+          "glue:GetDatabase",
+          "glue:GetTable",
+          "glue:CreateTable",
+          "glue:UpdateTable",
+          "glue:DeleteTable"
+        ]
+        Resource = [
+          "arn:aws:glue:*:605771322130:catalog",
+          "arn:aws:glue:*:605771322130:database/corretagem",
+          "arn:aws:glue:*:605771322130:table/corretagem/stock_data"
+        ]
+      },
+      
+      {
+        Sid    = "AthenaExecution"
+        Effect = "Allow"
+        Action = [
+          "athena:StartQueryExecution",
+          "athena:GetQueryExecution",
+          "athena:GetQueryResults",
+          "athena:StopQueryExecution"
+        ]
+        Resource = [
+          "arn:aws:athena:*:605771322130:workgroup/primary"
+        ]
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "attach_iceberg_policy" {
+  role       = aws_iam_role.lambda_execution_role.name
+  policy_arn = aws_iam_policy.lambda_iceberg_policy.arn
+}
