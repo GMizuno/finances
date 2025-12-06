db47d8e9287460188471528df85ca86ee4ada386
Gabriel Mizuno <gabrielmizuno@gmail.com>
2025-12-06 15:49:28 -0300

    Removes unused `handler` attribute from Lambda Terraform configuration.

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,7 +1,6 @@
 resource "aws_lambda_function" "lambda_stock_pricing" {
   function_name = "finance_stock"
   role          = aws_iam_role.lambda_execution_role.arn
-  handler       = "main.function_handler"
   kms_key_arn   = "arn:aws:kms:us-east-2:605771322130:key/d35f7bb6-58f5-478a-97f3-b89f8dcb1435"
   timeout       = 300
   description   = "Lambda para realizar operações de OCR nas notas de corretagem "
