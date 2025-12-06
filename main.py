cfa8c426df8cb568596ebab18f0d9e26268a4f55
Gabriel Mizuno <gabrielmizuno@gmail.com>
2025-12-05 22:06:49 -0300

    Removes legacy code and simplifies `main.py` by restructuring and reducing unused or redundant functionalities.

--- main.py
+++ main.py
@@ -1,50 +1,13 @@
-from loguru import logger
-
-import functions_framework
 import pendulum
-
-from src.bigquery.bigquery import run_query
-from src.message import parser_fail_msg, parser_sucess_msg, send_discord
-from src.message import month_over_month
-from src.util import PROJECT, QUERY, SELECT_TICKET, TABLE
-from src.util import etl
+import os
 
 
-@functions_framework.http
-def main(request):
-    request_json = request.get_json(silent=True)
-    tickets = request_json.get("tickets")
-    start = request_json.get("start")
-    end = request_json.get("end")
+def main() -> str:
+    print(pendulum.today())
 
-    logger.info(f"Extraindo dados dos seguintes ativos {tickets}")
-
-    if start is None:
-        start = pendulum.today().subtract(days=1).to_date_string()
-        end = pendulum.tomorrow().to_date_string()
-
-    logger.info(f"Extracting data from {start} to {end}")
-
-    for ticket in tickets:
-        try:
-            etl(ticket, start, end)
-        except Exception as e:
-            msg = parser_fail_msg(str(e), ticket)
-            send_discord(msg)
-            logger.error(f"Error ao inserir na tabela {TABLE}.\n{e}")
-
-    data_month_over_month = run_query(QUERY, PROJECT)
-    data_month_over_month = data_month_over_month[
-        data_month_over_month["Ticket"].isin(SELECT_TICKET)
-    ]
-
-    msg = parser_sucess_msg(tickets, start, end)
-    msg_month_over_month = month_over_month(
-        data_month_over_month.to_dict(orient="records")
-    )
-
-    send_discord(msg)
-    logger.info("Sending message with month over the month returns")
-    send_discord(msg_month_over_month)
+    print(os.listdir())
 
     return ""
+
+if __name__ == '__main__':
+    main()
