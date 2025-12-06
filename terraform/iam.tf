9cbebaee5bcd746c0e1c2eb585ad3be8d467d770
Gabriel Mizuno <gabrielmizuno@gmail.com>
2025-12-06 15:36:46 -0300

    Updates Terraform workflow to include `TF_VAR_webhook_url` environment variable and renames IAM role for Lambda deployment.

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_role" "lambda_execution_role" {
-  name               = "lambda_execution_role"
+  name               = "lambda_ecr_stock"
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
 
