fd777e9beecca0ae18b497af6a466522d9d7143d
Gabriel Mizuno <gabrielmizuno@gmail.com>
2026-01-01 21:34:53 -0300

    Refactor IAM policies to use `aws_iam_policy_document` for improved modularity and maintainability

--- terraform/data.tf
+++ terraform/data.tf
@@ -10,3 +10,86 @@ data "aws_iam_policy_document" "assume_role" {
     actions = ["sts:AssumeRole"]
   }
 }
+
+data "aws_iam_policy_document" "lambda_iceberg_policy_statement" {
+  statement {
+    sid    = "S3Access"
+    effect = "Allow"
+    actions = [
+      "s3:PutObject",
+      "s3:GetObject",
+      "s3:DeleteObject",
+      "s3:ListBucket",
+      "s3:GetBucketLocation"
+    ]
+    resources = [
+      "arn:aws:s3:::finance-605771322130",
+      "arn:aws:s3:::finance-605771322130/*"
+    ]
+  }
+
+  statement {
+    sid    = "GlueCatalogAccess"
+    effect = "Allow"
+    actions = [
+      "glue:GetDatabase",
+      "glue:GetTable",
+      "glue:CreateTable",
+      "glue:UpdateTable",
+      "glue:DeleteTable"
+    ]
+    resources = [
+      "arn:aws:glue:*:605771322130:catalog",
+      "arn:aws:glue:*:605771322130:database/corretagem",
+      "arn:aws:glue:*:605771322130:table/corretagem/*"
+    ]
+  }
+
+  statement {
+    sid    = "AthenaAccess"
+    effect = "Allow"
+    actions = [
+      "athena:StartQueryExecution",
+      "athena:GetQueryExecution",
+      "athena:GetQueryResults",
+      "athena:GetWorkGroup"
+    ]
+    resources = ["*"]
+  }
+
+  statement {
+    sid    = "SecretAccess"
+    effect = "Allow"
+    actions = [
+      "secretsmanager:GetSecretValue"
+    ]
+    resources = ["*"]
+  }
+
+  statement {
+    sid    = "S3AthenaResults"
+    effect = "Allow"
+    actions = [
+      "s3:GetBucketLocation",
+      "s3:GetObject",
+      "s3:ListBucket",
+      "s3:ListBucketMultipartUploads",
+      "s3:ListMultipartUploadParts",
+      "s3:AbortMultipartUpload",
+      "s3:CreateBucket",
+      "s3:PutObject"
+    ]
+    resources = [
+      "arn:aws:s3:::aws-athena-query-results-605771322130-us-east-2",
+      "arn:aws:s3:::aws-athena-query-results-605771322130-us-east-2/*"
+    ]
+  }
+}
+
+data "aws_iam_policy_document" "kms_decrypt_policy_statement" {
+  statement {
+    effect    = "Allow"
+    actions   = ["kms:Decrypt"]
+    resources = ["*"]
+  }
+}
--- terraform/iam.tf
+++ terraform/iam.tf
@@ -11,16 +11,7 @@ resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
 resource "aws_iam_policy" "kms_decrypt_policy" {
   name        = "kms_decrypt_policy_lambda_stock"
   description = "Permite desencriptação com uma KMS Key específica"
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action   = ["kms:Decrypt"]
-        Effect   = "Allow"
-        Resource = "*"
-      }
-    ]
-  })
+  policy      = data.aws_iam_policy_document.kms_decrypt_policy_statement.json
 }
 
 resource "aws_iam_role_policy_attachment" "kms_policy_attachment" {
@@ -32,79 +23,7 @@ resource "aws_iam_policy" "lambda_iceberg_policy" {
   name        = "lambda-iceberg-writer-policy"
   description = "Permissoes para escrever tabelas Iceberg via AWS Wrangler"
 
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Sid    = "S3Access"
-        Effect = "Allow"
-        Action = [
-          "s3:PutObject",
-          "s3:GetObject",
-          "s3:DeleteObject",
-          "s3:ListBucket",
-          "s3:GetBucketLocation"
-        ]
-        Resource = [
-          "arn:aws:s3:::finance-605771322130",  # O Bucket em si
-          "arn:aws:s3:::finance-605771322130/*" # Local Temporário/Staging
-        ]
-      },
-      {
-        Sid    = "GlueCatalogAccess"
-        Effect = "Allow"
-        Action = [
-          "glue:GetDatabase",
-          "glue:GetTable",
-          "glue:CreateTable",
-          "glue:UpdateTable",
-          "glue:DeleteTable"
-        ]
-        Resource = [
-          "arn:aws:glue:*:605771322130:catalog",
-          "arn:aws:glue:*:605771322130:database/corretagem",
-          "arn:aws:glue:*:605771322130:table/corretagem/*"
-        ]
-      },
-      {
-        "Sid" : "AthenaAccess",
-        "Effect" : "Allow",
-        "Action" : [
-          "athena:StartQueryExecution",
-          "athena:GetQueryExecution",
-          "athena:GetQueryResults",
-          "athena:GetWorkGroup"
-        ],
-        "Resource" : "*"
-      },
-      {
-        "Sid" : "SecretAcess",
-        "Effect" : "Allow",
-        "Action" : [
-          "secretsmanager:GetSecretValue"
-        ],
-        "Resource" : "*"
-      },
-      {
-        "Sid" : "S3AthenaResults",
-        "Effect" : "Allow",
-        "Action" : [
-          "s3:GetBucketLocation",
-          "s3:GetObject",
-          "s3:ListBucket",
-          "s3:ListBucketMultipartUploads",
-          "s3:ListMultipartUploadParts",
-          "s3:AbortMultipartUpload",
-          "s3:CreateBucket",
-          "s3:PutObject"
-        ],
-        "Resource" : [
-          "arn:aws:s3:::aws-athena-query-results-605771322130-us-east-2",
-          "arn:aws:s3:::aws-athena-query-results-605771322130-us-east-2/*"
-        ]
-      }
-    ]
-  })
+  policy = data.aws_iam_policy_document.lambda_iceberg_policy_statement.json
 }
 
 resource "aws_iam_role_policy_attachment" "attach_iceberg_policy" {
