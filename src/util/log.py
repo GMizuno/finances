15754dae9898837c8efa077285fd7536fe836a5e
Gabriel Mizuno <gabrielmizuno@gmail.com>
2026-01-01 21:35:46 -0300

    Simplify log format definition in `log.py`

--- src/util/log.py
+++ src/util/log.py
@@ -6,10 +6,6 @@ logger.remove()
 
 logger.add(
     sys.stdout,
-    format=(
-        "[<blue>{time:MMMM D, YYYY -> HH:mm:ss}</blue>] >> "
-        "<level>{level: <8}</level>: "
-        "<level>{message}</level>"
-    ),
+    format=("[{time:MMMM D, YYYY -> HH:mm:ss}</blue>] >> <level>{level: <8}</level>: <level>{message}</level>"),
     colorize=True,
 )
